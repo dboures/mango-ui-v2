ad6a46c0a6bc1531ee4301047397fed199f47f59
dboures <dboures@att.net>
2021-07-27 17:16:43 -0500

    temp commit

--- utils/chartDataConnector.tsx
+++ utils/chartDataConnector.tsx
@@ -1,12 +1,14 @@
 import { ChartTradeType } from '../@types/types'
 
-const baseUrl = 'https://serum-history.herokuapp.com'
+// const baseUrl = 'https://serum-history.herokuapp.com'
+const baseUrl = 'http://localhost:8080'
 export default class ChartApi {
   static URL = `${baseUrl}/`
 
   static async get(path: string) {
     try {
-      const response = await fetch(this.URL + path)
+      // const response = await fetch(this.URL + path)
+      const response = await fetch(path)
       if (response.ok) {
         const responseJson = await response.json()
         return responseJson.success
@@ -24,7 +26,8 @@ export default class ChartApi {
   static async getRecentTrades(
     marketAddress: string
   ): Promise<ChartTradeType[] | null> {
-    return ChartApi.get(`trades/address/${marketAddress}`)
+    // return ChartApi.get(`trades/address/${marketAddress}`)
+    return ChartApi.get(`https://serum-history.herokuapp.com/trades/address/${marketAddress}`)
   }
 
   static async getOhlcv(
@@ -34,7 +37,8 @@ export default class ChartApi {
     to: number
   ): Promise<ChartTradeType[] | null> {
     return ChartApi.get(
-      `tv/history?symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}`
+      // `tv/history?symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}`
+      `http://localhost:8080/tv/history?symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}`
     )
   }
 }
